65deef6fa37a37dd3038277a07bd3e4ee453ea89
sultanhan <2339334832@qq.com>
2019-03-07 16:58:15 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div class="hello">
+    <h1>这是helloworld的页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+ 
+    }
+  }
+  
+}
+</script>
+
